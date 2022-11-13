e5d3af253042165b674163a162e46e4e45354e5f
Hieser21 <rarafat883@gmail.com>
2022-11-13 02:18:55 +0000

    deleted:    .replit 	deleted:    env.json 	deleted:    index.html 	deleted:    replit.nix 	deleted:    script.js 	deleted:    style.css

--- script.js
+++
@@ -1,8 +0,0 @@
-// authenticates you with the API standard library
-// type `await lib.` to display API autocomplete
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-function getInputValue() {
-  let title = document.getElementById('title').value;
-  let desc = document.getElementById('description').value;
-  console.log(title);
-}
